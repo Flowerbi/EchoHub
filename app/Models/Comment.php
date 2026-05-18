71a672fb5933037cae4644e9bba46a56509d5898
Kirill <pritula_2005@mail.ru>
2026-05-19 03:30:27 +0500

    push project v.2

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Comment extends Model
 {
+    use HasFactory;
     protected $fillable = ['comment', 'author_id', 'post_id'];
 }
